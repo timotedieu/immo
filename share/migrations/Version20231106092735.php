1f9d389dc129207e305c6ea47e2b32fe586826b2
Dieu <timote.dieu@epsi.fr>
2023-11-06 10:28:18 +0100

    commit fin

---
+++ share/migrations/Version20231106092735.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231106092735 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE appartement (id INT AUTO_INCREMENT NOT NULL, supercficie VARCHAR(255) NOT NULL, descriptif VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, libelle_categorie VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE immeuble (id INT AUTO_INCREMENT NOT NULL, nom_imeuble VARCHAR(255) NOT NULL, rue_immeuble VARCHAR(255) NOT NULL, cp_imeuble VARCHAR(255) NOT NULL, ville_immeuble VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, date_reservation DATE NOT NULL, nomclient VARCHAR(255) NOT NULL, prenomclient VARCHAR(255) NOT NULL, rue_client VARCHAR(255) NOT NULL, cp_client VARCHAR(255) NOT NULL, ville_client VARCHAR(255) NOT NULL, tel_client VARCHAR(255) NOT NULL, num_cheque_acompte VARCHAR(255) NOT NULL, etat_resrvation VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE saison (id INT AUTO_INCREMENT NOT NULL, libelle_saison VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE semaine (id INT AUTO_INCREMENT NOT NULL, année DATE NOT NULL, num_semaine DATE NOT NULL, date_debut DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE appartement');
+        $this->addSql('DROP TABLE categorie');
+        $this->addSql('DROP TABLE immeuble');
+        $this->addSql('DROP TABLE reservation');
+        $this->addSql('DROP TABLE saison');
+        $this->addSql('DROP TABLE semaine');
+    }
+}
